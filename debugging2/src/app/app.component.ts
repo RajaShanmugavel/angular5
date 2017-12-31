2b27148461eab035d1f1694c51738a0f66986cc2
RajaShanmugavel <raja.karupiah@gmail.com>
2017-12-31 21:13:52 +1000

    another debug

--- debugging2/src/app/app.component.ts
+++ debugging2/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers;
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    const position = id + 1;
+    this.servers.splice(position, 1);
+  }
 }
