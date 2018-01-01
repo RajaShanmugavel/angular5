fa3555d5f4338d3b0ea41a5585ef1117dfc8a422
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-01 21:26:13 +1000

    Misc comment

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string}; // the argument 'srvElement' inside @Input is the alias. If not used, will be accessed using 'element'
 
   constructor() { }
 
