dc61f517e46a81a73cbe96dd9a13219fad787ec0
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-01 18:45:30 +1000

    custom property binding

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  newServerName = '';
+  newServerContent = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+    
+  onAddServer() {
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+  onAddBlueprint() {
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }  
 
 }
