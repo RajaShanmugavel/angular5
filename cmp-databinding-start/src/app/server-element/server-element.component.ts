0e303da55e131f3e9c610011db565de77b2856cf
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-06 22:08:45 +1000

    ngAfterViewInit and ngAfterViewChecked

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, DoCheck, AfterContentInit, AfterContentChecked } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +6,7 @@ import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, 
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // 'None' - Overrides style, also 'Native' (this does the shadow DOM). Default is 'Emulated'
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
   @Input('srvElement') element: { type: string, name: string, content: string }; // The argument 'srvElement' inside @Input is the alias. If not used, will be accessed using 'element'
   @Input() name: string;
@@ -32,6 +32,13 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
     console.log("ngAfterContentChecked of ServerElementComponent called!");
   }
 
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit of ServerElementComponent called!");
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked of ServerElementComponent called!");
+  }
 
   ngOnInit() {
     console.log("ngOnInit of ServerElementComponent called!");
