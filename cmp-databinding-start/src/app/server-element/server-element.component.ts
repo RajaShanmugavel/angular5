eae41e81b9cee77ebf5f2c11852a0633b386bbe8
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-07 21:24:05 +1000

    comp template access

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, ViewEncapsulation, SimpleChanges, 
-  OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
-  AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import {
+  Component, OnInit, Input, ViewEncapsulation, SimpleChanges,
+  OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -12,41 +14,45 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   @Input('srvElement') element: { type: string, name: string, content: string }; // The argument 'srvElement' inside @Input is the alias. If not used, will be accessed using 'element'
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
 
   constructor() {
     console.log("Constructor of ServerElementComponent called!");
   }
 
-  ngOnChanges(changes : SimpleChanges){
-    console.log("ngOnChanges of ServerElementComponent called!");
-    console.log("Changes:"+changes);
+  ngOnInit() {
+    console.log("ngOnInit of ServerElementComponent called!");
+    console.log('Text Content:'+this.header.nativeElement.textContent); 
   }
 
-  ngDoCheck(){
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges of ServerElementComponent called!");
+    console.log("Changes:" + changes);
+  }
+
+  ngDoCheck() {
     console.log("ngDoCheck of ServerElementComponent called!");
   }
 
-  ngAfterContentInit(){
+  ngAfterContentInit() {
     console.log("ngAfterContentInit of ServerElementComponent called!");
   }
 
-  ngAfterContentChecked(){
+  ngAfterContentChecked() {
     console.log("ngAfterContentChecked of ServerElementComponent called!");
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     console.log("ngAfterViewInit of ServerElementComponent called!");
+    console.log('Text Content:'+this.header.nativeElement.textContent); 
   }
 
-  ngAfterViewChecked(){
+  ngAfterViewChecked() {
     console.log("ngAfterViewChecked of ServerElementComponent called!");
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     console.log("ngOnDestroy of ServerElementComponent called!");
   }
 
-  ngOnInit() {
-    console.log("ngOnInit of ServerElementComponent called!");
-  }
 }
