5ea08fde51ed536c1c0da02fdf4f8966a0571880
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-08 21:16:35 +1000

    @contentchild

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, OnInit, Input, ViewEncapsulation, SimpleChanges,
   OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
-  AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef
+  AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild
 } from '@angular/core';
 
 @Component({
@@ -15,6 +15,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   @Input('srvElement') element: { type: string, name: string, content: string }; // The argument 'srvElement' inside @Input is the alias. If not used, will be accessed using 'element'
   @Input() name: string;
   @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() {
     console.log("Constructor of ServerElementComponent called!");
@@ -23,6 +24,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngOnInit() {
     console.log("ngOnInit of ServerElementComponent called!");
     console.log('Text Content:'+this.header.nativeElement.textContent); 
+    console.log('Text Content of Paragraph:'+this.paragraph.nativeElement.textContent);
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -36,6 +38,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngAfterContentInit() {
     console.log("ngAfterContentInit of ServerElementComponent called!");
+    console.log('Text Content of Paragraph:'+this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
