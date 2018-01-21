ecb72644b86ec26d4c35d0fc323bbf9730e0a978
RajaShanmugavel <raja.karupiah@gmail.com>
2018-01-21 20:04:20 +1000

    basic custom directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BasicHightlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHightlightDirective
+    BasicHightlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -8,6 +8,6 @@ export class BasicHightlightDirective implements OnInit{
     }
 
     ngOnInit(){
-        this.elementRef.nativeElement.style.backgroundColor = 'green';
+        this.elementRef.nativeElement.style.backgroundColor = 'lightgreen';
     }
 }
