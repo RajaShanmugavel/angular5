8320a4e6817fb6f0579d2fe7ea2b02d86942d722
RajaShanmugavel <raja.karupiah@gmail.com>
2018-03-07 21:47:01 +1000

    routing common practices

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -15,11 +15,11 @@ export class ServerComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params
       .subscribe(params => {
-        this.server = this.serversService.getServer(params['id']);
+        this.server = this.serversService.getServer(+params['id']);
       });
   }
 
