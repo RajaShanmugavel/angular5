d2d9082eb523b41c1a1a675e0a1cad16f76ecae2
RajaShanmugavel <raja.karupiah@gmail.com>
2018-03-28 21:57:12 +1000

    routing with children

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -21,9 +21,13 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent }
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  }
+
 ];
 
 @NgModule({
