7cf67ee5a9b03f2e26666a7dc55d0ade08a952aa
RajaShanmugavel <raja.karupiah@gmail.com>
2018-05-01 21:02:52 +1000

    handle spring security

--- myhomeproject/src/app/login.service.ts
+++ myhomeproject/src/app/login.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Employee } from './employee';
+import { RequestOptions, Headers } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
+
+import { Employee } from './employee';
 
 @Injectable()
 export class LoginService {
 
   url = 'http://localhost:8080/show-time';
+
+  headers = new Headers({
+    'Authorization': 'Basic ',
+    'X-Requested-With': 'XMLHttpRequest' // to suppress 401 browser popup
+  });
+
+  options = new RequestOptions({
+    headers: this.headers
+  });
 
   constructor(private http: HttpClient) {
 
