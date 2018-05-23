73675033c29290a6102203e60157046c2eb6b473
RajaShanmugavel <raja.karupiah@gmail.com>
2018-05-23 21:28:55 +1000

    adding style to mat tab

--- myhomeproject/src/app/employee/employee.component.ts
+++ myhomeproject/src/app/employee/employee.component.ts
@@ -4,7 +4,8 @@ import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee',
-  templateUrl: './employee.component.html'
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
 
