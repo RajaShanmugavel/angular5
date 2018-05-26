939f3e031f09379a73165dba433c0db113255553
RajaShanmugavel <raja.karupiah@gmail.com>
2018-05-26 20:26:20 +1000

    updates to reactive forms

--- myhomeproject/src/app/employee-form/employee-form.component.ts
+++ myhomeproject/src/app/employee-form/employee-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-form',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeFormComponent implements OnInit {
 
-  constructor() { }
+  employeeForm: FormGroup;
+  employee = new Employee();
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      empId: '',
+      salary: ''
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('employeeForm valid:' + this.employeeForm.valid);
+    this.employee = this.employeeForm.value;
+    console.log('this.employee:' + this.employee.empId);
   }
 
 }
