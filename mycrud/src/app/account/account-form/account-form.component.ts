c42e1594cd24a0c91279d2464aa3771d2c011e44
RajaShanmugavel <raja.karupiah@gmail.com>
2018-12-23 20:41:35 +1000

    minor updates to form

--- mycrud/src/app/account/account-form/account-form.component.ts
+++ mycrud/src/app/account/account-form/account-form.component.ts
@@ -1,33 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Account } from '../account';
 
 @Component({
   selector: 'app-account-form',
   templateUrl: './account-form.component.html',
   styleUrls: ['./account-form.component.css']
 })
-export class AccountFormComponent implements OnInit {
+export class AccountFormComponent implements OnInit, OnChanges {
 
   accountForm: FormGroup;
+  account: Account = new Account();
 
   constructor(private fb: FormBuilder) {
     this.createForm();
-   }
+  }
 
-   createForm() {
+  createForm() {
     this.accountForm = this.fb.group({
       id: null,
       name: null,
       dob: null,
       gender: null
     });
-   }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
+
+  ngOnChanges() {}
 
   onSubmit() {
-
+    this.account = this.accountForm.value;
+    console.log(this.account);
   }
-
 }
---
+++ mycrud/src/app/account/account.ts
@@ -0,0 +1,6 @@
+export class Account {
+  id: number;
+  name: string;
+  dob: Date = new Date();
+  gender: string;
+}
