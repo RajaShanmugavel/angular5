0f8be621341d531bbb1921a9851c968782b013b3
RajaShanmugavel <raja.karupiah@gmail.com>
2020-05-14 12:25:37 +1000

    adding components

---
+++ ang9-may2020/src/app/cars/cars-form/cars-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-form',
+  templateUrl: './cars-form.component.html',
+  styleUrls: ['./cars-form.component.css']
+})
+export class CarsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ang9-may2020/src/app/cars/cars-table/cars-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-table',
+  templateUrl: './cars-table.component.html',
+  styleUrls: ['./cars-table.component.css']
+})
+export class CarsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
