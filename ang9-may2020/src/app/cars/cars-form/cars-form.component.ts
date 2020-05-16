cdf90ff71d62621cba18d621f33704c3e42149c3
RajaShanmugavel <raja.karupiah@gmail.com>
2020-05-16 19:18:52 +1000

    form

--- ang9-may2020/src/app/cars/cars-form/cars-form.component.ts
+++ ang9-may2020/src/app/cars/cars-form/cars-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cars-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsFormComponent implements OnInit {
 
-  constructor() { }
+  carsForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.carsForm = this.fb.group({
+
+    });
   }
 
 }
