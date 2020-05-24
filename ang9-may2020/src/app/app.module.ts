5fbff1bb6132a69ec8d3254b666d536738fb9c5b
RajaShanmugavel <raja.karupiah@gmail.com>
2020-05-24 20:13:20 +1000

    misc

--- ang9-may2020/src/app/app.module.ts
+++ ang9-may2020/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -16,16 +16,16 @@ import { routingModule } from './routingModule';
     AppComponent,
     CarsComponent,
     CarsFormComponent,
-    CarsTableComponent
+    CarsTableComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     routingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
