e3550537c5051bb6377baafa789b51f0647c8a10
RajaShanmugavel <raja.karupiah@gmail.com>
2020-08-05 20:22:41 +1000

    misc

--- ang9-may2020/src/app/routingModule.ts
+++ ang9-may2020/src/app/routingModule.ts
@@ -2,9 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsComponent } from './cars/cars.component';
 
-const routes : Routes = [
-  {path: 'cars', component: CarsComponent}
-];
+const routes: Routes = [{ path: 'cars', component: CarsComponent }];
 
 /**
  * TODO ... add routers..
